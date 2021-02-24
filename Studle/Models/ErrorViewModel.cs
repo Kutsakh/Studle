df18eb59346e16f1d2ea24fb2222408bb9022e3a
Rostyslav Begey <rostyk.begey@gmail.com>
2021-02-24 15:37:18 +0200

    Initial commit

---
+++ Studle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Studle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
