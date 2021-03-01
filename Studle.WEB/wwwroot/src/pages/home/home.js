70c29c95f20224f7ec1b3ff6f3f6012d05903d08
Rostyslav Begey <rostyk.begey@gmail.com>
2021-03-01 18:27:55 +0200

    Update redux setup. Add code style tools. Update webpack config.

--- Studle.WEB/wwwroot/src/pages/home/home.js
+++ Studle.WEB/wwwroot/src/pages/home/home.js
@@ -1,41 +1,28 @@
-import { createStore } from "redux";
+import { $ } from '../../utils/common';
+import initPage from '../../utils/initPage';
 
-const initialState = {
-    counter: 0,
+import store from './store';
+import { decrement, increment } from './actions';
+
+import './home.scss';
+
+const render = (prevState, state) => {
+  const { counter } = state;
+  const { prevCounter = {} } = prevState || {};
+
+  if (prevCounter.value !== counter.value) {
+    $('#counter').innerText = counter.value.toString();
+  }
+
+  if (!prevState) {
+    $('#increment').addEventListener('click', () =>
+      store.dispatch(increment()),
+    );
+
+    $('#decrement').addEventListener('click', () =>
+      store.dispatch(decrement()),
+    );
+  }
 };
 
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-
-const increment = () => ({ type: INCREMENT });
-const decrement = () => ({ type: DECREMENT });
-
-const reducer = (state = initialState, { type, payload }) => {
-    switch (type) {
-       case INCREMENT:
-           return { ...state, counter: state.counter + 1 };
-       case DECREMENT:
-           return { ...state, counter: state.counter - 1 };
-       default:
-           return { ...state };
-   }
-};
-
-const store = createStore(reducer, initialState, undefined);
-
-const render = () => {
-    const { counter } = store.getState();
-    console.log(store.getState());
-    document.getElementById('counter').innerText = counter;
-};
-
-store.subscribe(render);
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('increment').addEventListener('click', () => {
-        store.dispatch(increment());
-    });
-    document.getElementById('decrement').addEventListener('click', () => {
-        store.dispatch(decrement());
-    });
-});
+initPage(store, render);
--- Studle.WEB/wwwroot/webpack.config.js
+++ Studle.WEB/wwwroot/webpack.config.js
@@ -1,12 +1,27 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        home: './pages/home/home.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist/pages'),
-        filename: '[name].js',
-    },
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    home: './pages/home/home.js',
+  },
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, 'dist/pages'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 };
