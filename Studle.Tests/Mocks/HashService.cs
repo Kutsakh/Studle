d853c451763cb5154aa42e135508d51bd4350fc9
Kutsakh <a.kutsach@gmail.com>
2021-04-01 20:37:39 +0300

    Create a Tests

---
+++ Studle.Tests/Mocks/HashService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Studle.BLL.Interfaces;
+
+namespace Studle.Tests.Mocks
+{
+    class HashService : IHashService
+    {
+        public string GetHash(string password)
+        {
+            return password;
+        }
+    }
+}
---
+++ Studle.Tests/Mocks/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Studle.DAL.Entities;
+using Studle.DAL.Interfaces;
+
+namespace Studle.Tests.Mocks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IRepository<User> Users { get; }
+
+        public IRepository<Mark> Marks { get; }
+
+        public IRepository<Subject> Subjects { get; }
+
+       
+
+        public UnitOfWork()
+        {
+            Users = new MockRepository<User>();
+
+            Marks = new MockRepository<Mark>();
+
+            Subjects = new MockRepository<Subject>();
+            
+        }
+
+        public void Save() { }
+
+        public void Dispose() { }
+    }
+}
