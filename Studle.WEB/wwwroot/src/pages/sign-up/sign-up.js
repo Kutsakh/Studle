e0f5d46c8169fb78c827906909be26af235d2c97
Rostyslav Begey <rostyk.begey@gmail.com>
2021-05-13 13:09:50 +0300

    Update sign-up.js script

--- Studle.WEB/wwwroot/src/pages/sign-up/sign-up.js
+++ Studle.WEB/wwwroot/src/pages/sign-up/sign-up.js
@@ -6,9 +6,9 @@ import '../../sections/header/hader';
 import './sign-up.scss';
 
 const emailInput = $('[name="email"]');
-const nameInput = $('[name="name"]');
+const nameInput = $('[name="userName"]');
 const passwordInput = $('[name="password"]');
-const confirmPasswordInput = $('[name="confirm-password"]');
+const confirmPasswordInput = $('[name="confirmPassword"]');
 const formElement = $('form');
 const formErrorElement = $('#form-error');
 const submitButton = $('button[type="submit"]');
@@ -17,13 +17,8 @@ const formInputs = $$('form input');
 const schema = yup.object().shape({
   name: yup.string().required('Name is required'),
   email: yup.string().email('Email is invalid').required('Email is required'),
-  password: yup
-    .string()
-    .min(8, 'Password must be at least 8 characters')
-    .required('Password is required'),
-  'confirm-password': yup
-    .string()
-    .oneOf([yup.ref('password'), null], "Passwords didn't match"),
+  password: yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
+  confirmPassword: yup.string().oneOf([yup.ref('password'), null], "Passwords didn't match"),
 });
 
 resetAuthFormError(formInputs, formErrorElement);
@@ -35,7 +30,7 @@ submitButton.addEventListener('click', (e) => {
     name: nameInput.value,
     email: emailInput.value,
     password: passwordInput.value,
-    'confirm-password': confirmPasswordInput.value,
+    confirmPassword: confirmPasswordInput.value,
   };
 
   schema
